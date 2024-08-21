71f53ab1e7b67c41b86ee2455ddd44733a279ac5
Gustavo Dias <dias.gustavof@gmail.com>
2024-08-21 13:12:45 -0300

    Atualização do index.html e criação do acount.html

--- routes/client.py
+++ routes/client.py
@@ -1,5 +1,7 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 
 client_route = Blueprint("client", __name__)
 
-# @client_route.route("/")
+@client_route.route("/acount")
+def acount():
+    return render_template("acount.html")
--- routes/home.py
+++ routes/home.py
@@ -1,3 +1,7 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 
 home_route = Blueprint("home", __name__)
+
+@home_route.route('/')
+def home():
+    return render_template("index.html")
