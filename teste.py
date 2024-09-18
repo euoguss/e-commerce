1fac807e42d79f74f455aba7f6d7a1fc322da107
Matheus38652 <silva.santos13652@gmail.com>
2024-09-18 20:26:33 -0300

    reza e acredita

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ from database.database import BancoDeDados
 db = BancoDeDados()
 db.connect()
 
-val = ('dias.gustavof@gmail.com', '8127347897289')
+val = ('dias.gustavof@gmail.com', '81273dkvjbjksdvknjbaf47897289')
 resul = (db.singin(val))
 db.verify()
 
