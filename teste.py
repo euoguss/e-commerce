e87aae355d66b9bd7b1916957b7aa761bc987dfe
Matheus38652 <silva.santos13652@gmail.com>
2024-09-19 13:34:22 -0300

    Implementação da pagina dos produtos

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ from database.database import BancoDeDados
 db = BancoDeDados()
 db.connect()
 
-val = ('dias.gustavof@gmail.com', '81273dkvjbjksdvknjbaf47897289')
+val = ('silva.santos13652@gmail.com', '81273dkvjbjksdvknjbaf47897289')
 resul = (db.singin(val))
 db.verify()
 
