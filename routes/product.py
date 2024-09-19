34e5a16fd1b575eb9cae2d059c89f51c921f2e76
Matheus38652 <silva.santos13652@gmail.com>
2024-09-18 23:24:37 -0300

    Complementado css na pagina de produto

--- routes/product.py
+++ routes/product.py
@@ -5,13 +5,6 @@ product_route = Blueprint("product", __name__)
 @product_route.route("/add")
 def add_prod():
     # adicionar produtos no site - post
-    return render_template('form_prod.html')from flask import Flask, render_template, Blueprint
-
-product_route = Blueprint("product", __name__)
-
-@product_route.route("/add")
-def add_prod():
-    # adicionar produtos no site - post
     return render_template('form_prod.html')
 
 @product_route.route("/gab")
